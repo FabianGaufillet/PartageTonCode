98628deb305186a477396a95652421b0694b5f18
Fabian Gaufillet <fabian.gaufillet@gmail.com>
2025-01-25 16:58:05 +0100

    Gestion du SSL/TLS pour l'envoi de mail

--- back/config/nodemailer.js
+++ back/config/nodemailer.js
@@ -4,7 +4,7 @@ import { SMTP_PORT, SMTP_HOST, MAILBOX_USER, MAILBOX_PASSWORD } from "./env.js";
 export default nodemailer.createTransport({
   host: SMTP_HOST,
   port: SMTP_PORT,
-  secure: false,
+  secure: SMTP_PORT === 465,
   auth: {
     user: MAILBOX_USER,
     pass: MAILBOX_PASSWORD,
